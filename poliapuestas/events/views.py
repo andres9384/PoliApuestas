1db0d57dd69b7d38ea51bcebb9dc95b491695817
Andres Cristancho <andrescristancho46@gmail.com>
2023-10-24 16:30:46 -0500

    comentarios proyecto

--- poliapuestas/events/views.py
+++ poliapuestas/events/views.py
@@ -6,16 +6,34 @@ from .models import *
 from .serializers import *
 
 
+
+
 class BetModelViewSet(viewsets.ModelViewSet):
+    """
+        API endpoint que permite ver, crear, actualizar o eliminar apuestas.
+
+        :param models.Bet ViewSet.ModelViewSet
+        :returns serializer_class BetSerializer
+    """
     queryset = Bet.objects.all()
     serializer_class = BetSerializer
 
 
 class CategoryModelViewSet(viewsets.ModelViewSet):
+    """
+        API endpoint que permite ver, crear, actualizar o eliminar una categoria.
+        :param models.Category ViewSet.ModelViewSet
+        :returns serializer_class CategorySerializer
+    """
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
 
 
 class RaffleModelViewSet(viewsets.ModelViewSet):
+    """
+        API endpoint que permite ver, crear, actualizar o eliminar Rifas .
+        :param models.Raffle ViewSet.ModelViewSet
+        :returns serializer_class RaffleSerializer
+    """
     queryset = Raffle.objects.all()
     serializer_class = RaffleSerializer
